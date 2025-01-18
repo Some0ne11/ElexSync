55fc49a3f3339aa2676ca4edd0edbc1d5559678f
Muhammad Luqman <luqmanrasimi@student.usm.my>
2025-01-19 00:12:39 +0800

    update product.jsp

--- src/main/java/cat201/cat201project/controller/HomepageServlet.java
+++ src/main/java/cat201/cat201project/controller/HomepageServlet.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 @WebServlet (name="HomepageServlet", urlPatterns = "/home")
 public class HomepageServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/collection.jsp").forward(request, response);
+        getServletContext().getRequestDispatcher("/product_temp.jsp").forward(request, response);
     }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
