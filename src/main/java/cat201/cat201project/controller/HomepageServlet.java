c10e99f9d2a0d2aaee97b33a2e2c6c7b86558469
Asyrafmika <ahmadasyraf30181@gmail.com>
2025-01-18 23:41:57 +0800

    I add all the necessary file

--- src/main/java/cat201/cat201project/controller/HomepageServlet.java
+++ src/main/java/cat201/cat201project/controller/HomepageServlet.java
@@ -1,6 +1,5 @@
 package cat201.cat201project.controller;
 
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,13 +11,8 @@ import java.io.IOException;
 @WebServlet (name="HomepageServlet", urlPatterns = "/home")
 public class HomepageServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-<<<<<<< HEAD
         getServletContext().getRequestDispatcher("/homepage.jsp").forward(request, response);
-=======
-        getServletContext().getRequestDispatcher("/collection.jsp").forward(request, response);
->>>>>>> 7562c66cc7d82ef7208636a73934500e39d668ad
     }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
     }
 }
